27f8aa0ad09f2d2c18b17dc8210282363382cfe5
sarafa19 <diomandasf@gmail.com>
2025-06-23 22:10:54 +0200

    pr

--- frontend/server.js
+++ frontend/server.js
@@ -1,4 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('The SARAH Frontend Running'));
+app.get('/', (req, res) => res.send('The SARAH FATIM Frontend Running'));
 app.listen(3000, () => console.log('Frontend on port 3000'));
